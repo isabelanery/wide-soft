c1a8005a828e6967cb165d93398634f956e0bf92
Isabela Nery <isabelanery.r@gmail.com>
2022-09-02 11:27:23 -0300

    feat: Table structure

--- resources/js/components/urlList/Table.js
+++ resources/js/components/urlList/Table.js
@@ -1,10 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function Table() {
-    return (
-        <>ALO</>
-    );
+class Table extends React.Component {
+    
+
+    render() {
+        return (
+            <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-md-8">
+                    <div className="card">
+                        
+                        <table className="table table-hover">
+                            <thead>
+                                <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">URL</th>
+                                <th scope="col">Response</th>
+                                <th scope="col">HTML</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                <th scope="row">1</th>
+                                <td>Mark</td>
+                                <td>Otto</td>
+                                <td>@mdo</td>
+                                </tr>
+                                
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        );
+    }
 }
 
 export default Table;
