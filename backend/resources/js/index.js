c4b15c454c83b32e998b829cad035412c8aba395
Isabela Nery <isabelanery.r@gmail.com>
2022-09-05 20:55:29 -0300

    reset

--- backend/resources/js/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-
-if (document.getElementById('wideSoft')) {
-  ReactDOM.render(<App />, document.getElementById('wideSoft'));
-}
